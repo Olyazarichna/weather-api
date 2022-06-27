4d5c5edd8a2cba85187afb129ec28d59ab06d442
Olyazarichna <olya.zarichna@gmail.com>
2022-06-27 10:35:52 +0300

    change my js code

--- src/index.js
+++ src/index.js
@@ -7,32 +7,24 @@ const inputEl = document.querySelector(".search-field");
 const cityName = document.querySelector(".city-name");
 const currentLoc = document.querySelector(".location");
 const iconEl = document.querySelector(".icon-title");
+const temperature = document.querySelector(".text-title");
 
 formEl.addEventListener("submit", onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
   let city = inputEl.value;
-
-  if (!city) {
+  if (city === "") {
     return;
   }
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
-
-  cityName.innerHTML = city;
-
-  axios.get(url).then(showWeather);
   event.currentTarget.reset();
+  searchCity(city);
 }
 
 function showWeather(response) {
-  if (!response.data.name) {
-    return;
-  }
-
+  cityName.innerHTML = response.data.name;
   let temp = Math.round(response.data.main.temp);
-  const tempEl = document.querySelector(".text-title");
-  tempEl.innerHTML = temp;
+  temperature.innerHTML = temp;
   if (temp >= 20) {
     iconEl.innerHTML = "☀️";
   } else if (temp < 0) {
@@ -40,27 +32,28 @@ function showWeather(response) {
   } else if (temp > 5 && 19) {
     iconEl.innerHTML = "🌥";
   }
-  let wind = response.data.wind.speed;
   const windEl = document.querySelector(".wind");
   cityName.innerHTML = response.data.name;
-  windEl.innerHTML = `Wind speed: ${wind}`;
-  console.log(temp);
+  windEl.innerHTML = `Wind speed: ${response.data.wind.speed}`;
 }
 
 currentLoc.addEventListener("click", onBtnClick);
 
 function onBtnClick(event) {
   event.preventDefault();
-  console.log("current location");
   navigator.geolocation.getCurrentPosition(showLocation);
 }
 
-navigator.geolocation.getCurrentPosition(showLocation);
-
 function showLocation(position) {
-  console.log(position);
   let lat = position.coords.latitude;
   let long = position.coords.longitude;
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}&units=metric`;
   axios.get(url).then(showWeather);
 }
+
+function searchCity(city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+  axios.get(url).then(showWeather);
+}
+
+searchCity("Kyiv");
